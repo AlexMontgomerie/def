a6923bc91485301a628c95190a57b80b4bfdae3d
AlexMontgomerie <AlexMontgomerie@github.com>
2020-06-24 13:20:20 +0100

    working on hardware tests

--- hw/src/test/scala/deaf/DEAFTester.scala
+++ hw/src/test/scala/deaf/DEAFTester.scala
@@ -2,4 +2,15 @@ package deaf.test
 
 import deaf._
 
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
+class int_to_sint_tester(c: int_to_sint) extends PeekPokeTester(c) {
+
+
+}
+
+class DEAFTester extends ChiselFlatSpec {
+  // int to sint unit tester
+    Driver(() => new int_to_sint(8), "verilator") { c => new int_to_sint_tester(c) } should be (true)
+}
