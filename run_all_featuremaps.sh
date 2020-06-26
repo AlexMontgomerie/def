7f5f64f3bb4e161f665033bb158715a27080bb5b
AlexMontgomerie <am9215@ic.ac.uk>
2020-06-26 15:58:20 +0100

    added updated output metrics

--- run_all_featuremaps.sh
+++ run_all_featuremaps.sh
@@ -2,7 +2,8 @@
 LIMIT=100000
 
 #mkdir -p outputs/caffe_alexnet_8b  && python -m scripts.run_featuremap -n alexnet -f featuremaps/caffe_alexnet_8b.h5  -o outputs/caffe_alexnet_8b  -l $LIMIT -b 8
-mkdir -p outputs/caffe_alexnet_8b  && python -m scripts.run_featuremap_multi -n alexnet -f featuremaps/caffe_alexnet_8b.h5  -o outputs/caffe_alexnet_8b  -l $LIMIT -b 8
+mkdir -p outputs/distiller_alexnet_8b  && python -m scripts.run_featuremap -n alexnet -f featuremaps/distiller_alexnet_8b.h5  -o outputs/distiller_alexnet_8b  -l $LIMIT -b 8
+#mkdir -p outputs/caffe_alexnet_8b  && python -m scripts.run_featuremap_multi -n alexnet -f featuremaps/caffe_alexnet_8b.h5  -o outputs/caffe_alexnet_8b  -l $LIMIT -b 8
 #mkdir -p outputs/caffe_alexnet_16b && python -m scripts.run_featuremap -n alexnet -f featuremaps/caffe_alexnet_16b.h5 -o outputs/caffe_alexnet_16b -l $LIMIT -b 8
 
 
